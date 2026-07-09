1df24fa52c1f5156243cb766212a06f0433bd438
JordonJOJO <jordonyeon75@gmail.com>
2026-07-09 15:58:37 +0800

    Delete EcoNavigator directory

--- EcoNavigator/api/get_ratings.php
+++
@@ -1,7 +0,0 @@
-<?php
-// api/get_ratings.php
-require_once __DIR__ . '/../model/LocationModel.php';
-$model = new LocationModel();
-
-$locationId = intval($_GET['location_id']);
-echo json_encode($model->getAverageRating($locationId));
